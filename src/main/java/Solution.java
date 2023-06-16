953e76315afed1eba46210e6654c1deb9efec490
alek <wait82985@gmail.com>
2023-06-16 18:14:38 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,18 @@
 public class Solution {
     public int change(int amount, int[] coins) {
-        //TODO
-        return 0;
+        int[][] dp = new int[coins.length][amount + 1];
+        for (int i = 0; i < coins.length; i++) {
+            dp[i][amount] = 1;
+        }
+        for (int i = coins.length - 1; i >= 0; i--) {
+            for (int j = amount - 1; j >= 0; j--) {
+                if (i == coins.length - 1) {
+                    dp[i][j] = j + coins[i] > amount ? 0 : dp[i][j + coins[i]];
+                } else {
+                    dp[i][j] = (j + coins[i] > amount ? 0 : dp[i][j + coins[i]]) + dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
